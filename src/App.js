9f227b3e38816c92b4cebef96353c0e3665eadcf
muminovaziz <muminov.me.2001@gmail.com>
2023-04-06 09:26:28 +0500

    added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useState } from "react";
 function App() {
+  const [counter, setCounter] = useState(0)
+  function inscerement() {
+    if (counter <= 98) {
+      setCounter(counter + 1)
+
+    }
+  }
+  function decrement() {
+    if (counter > 0) {
+      setCounter(counter - 1)
+    }
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="App" >
+      <button onClick={inscerement}>+</button>
+      <h1>{counter}</h1>
+      <button onClick={decrement}>-</button>
+    </div >
   );
 }
 
